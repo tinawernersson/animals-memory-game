2046a97aa01ecee9709b0eebdbe3e0f27545c867
Tina Wernersson <tinawernersson@hotmail.com>
2023-12-25 15:01:34 +0000

    add variables in script.js file, add basic functions with no code in script.js

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function startGame () {
+// Variables
+const startButton = document.getElementById('start');
+const restartButton = document.getElementById('restart');
+const moves = document.getElementById('move');
+const timer = document.getElementById('time');
+const catCard = document.getElementById('cat');
+const cowCard = document.getElementById('cow');
+const dogCard = document.getElementById('dog');
+const elephantCard = document.getElementById('elephant');
+const guineapigCard = document.getElementById('guinea-pig');
+const horseCard = document.getElementById('horse');
+const lambCard = document.getElementById('lamb');
+const butteflyfishCard = document.getElementById('butterfly-fish');
+const lionCard = document.getElementById('lion');
+const monkeyCard = document.getElementById('monkey');
+const mouseCard = document.getElementById('mouse');
+const owlCard = document.getElementById('owl');
 
+//Event listeners
+startButton.addEventListener('click', startGame);
+restartButton.addEventListener('click', restartGame); {
+    location.reload();
+  }, false);
+
+
+
+let time = 0;
+
+function startGame () {
+    cardArray.sort ();
 }
 
 function flipCard() {
@@ -7,7 +35,7 @@ function flipCard() {
 }
 
 function correctPair() {
-
+       
 }
 
 function wrongPair() {
@@ -19,5 +47,6 @@ function restartGame() {
 }
 
 function timeCounter() {
+
+      }
     
-}
